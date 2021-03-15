85f604b4c2ffec3ef8f3c8df6f230c30eb9e84d8
Larah-Lira <larah.karolyne@wlcsolucoes.com.br>
2021-03-15 13:53:57 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    <p>Olá mundo</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
